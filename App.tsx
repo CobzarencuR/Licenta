4841695ce535022b552ef390ad07c5052afdf225
CobzarencuR <robert.cobzarencu.rc33@gmail.com>
2025-02-19 22:19:38 +0200

    branch test

--- App.tsx
+++ App.tsx
@@ -9,24 +9,6 @@ import React from 'react';
 import AppNavigator from './src/navigation/AppNavigator';
 import { useEffect } from 'react';
 
-import {
-    SafeAreaView,
-    ScrollView,
-    StatusBar,
-    StyleSheet,
-    Text,
-    useColorScheme,
-    View,
-} from 'react-native';
-
-import {
-    Colors,
-    DebugInstructions,
-    Header,
-    LearnMoreLinks,
-    ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
 function App() {
     return (
         <AppNavigator />
