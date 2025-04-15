778b9095334db490a58fc5a7b052aa0282c7ce99
CobzarencuR <robert.cobzarencu.rc33@gmail.com>
2025-04-15 20:05:50 +0300

    finally made settings icon appear in header

--- src/components/SettingsButton.tsx
+++ src/components/SettingsButton.tsx
@@ -3,12 +3,11 @@ import { TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
-
 export default function SettingsButton() {
     const navigation = useNavigation();
     return (
         <TouchableOpacity onPress={() => navigation.navigate('Settings' as never)}>
-            <Icon name="settings-outline" size={24} color="white" />
+            <Icon name="settings-outline" size={26} color="white" />
         </TouchableOpacity>
     );
 };
